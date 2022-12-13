23058631b3af88787d62eebf12ee47fca392a6ed
Aamir Khan <aamirkhan-1@outlook.com>
2022-12-13 19:57:07 +0530

    feat: ✨ Configs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// import { fileURLToPath, URL } from 'node:url';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { manualChunksPlugin } from 'vite-plugin-webpackchunkname';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,21 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src'),
-            // '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                visualizer({
+                    template: 'sunburst',
+                    filename: 'build-stats.html',
+                    gzipSize: true,
+                    brotliSize: true,
+                }),
+            ],
+        },
+    },
+    esbuild: {
+        drop: ['console', 'debugger'],
     },
 });
